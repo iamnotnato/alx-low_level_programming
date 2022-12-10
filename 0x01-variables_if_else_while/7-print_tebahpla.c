bafead4aed1b9cf0e45cf5469d296e3b44ffd283
Alan Nato <50413623+iamnotnato@users.noreply.github.com>
2022-12-10 07:26:49 -0600

    Betty Linter

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,11 +4,11 @@
  * Description : Prints the lowercase alphabet in reverse.
  * Return: Always 0.
  */
-int main(void)  
+int main(void)
 {
 char letter;
 for (letter = 'z'; letter >= 'a'; letter--)
 putchar(letter);
 putchar('\n');
-return (0);  
+return (0);
 }
