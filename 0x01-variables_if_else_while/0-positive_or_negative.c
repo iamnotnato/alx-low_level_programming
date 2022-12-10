f9b788d95c8fdcb791901c2e1611c2c916058579
Alan Nato <alanwnato@gmail.com>
2022-12-09 20:04:02 -0800

    Last Last

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,11 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
- if (n == 0)
-    printf("%d is zero\n", n);
- else if (n < 0)
-    printf("%d is negative\n", n);
- else
-    printf("%d is positive\n", n);
- return (0);
- }
+if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is positive\n", n);
+return (0);
+}
