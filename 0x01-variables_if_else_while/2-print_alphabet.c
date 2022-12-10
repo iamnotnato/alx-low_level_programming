de376bb6434b64e1877b40008930d2a4b0726dbf
Alan Nato <alanwnato@gmail.com>
2022-12-09 20:10:02 -0800

    Betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,13 @@
 #include <stdio.h>
-
 /**
-
  * main - Prints the alphabet
-
- *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-  
 {
-  
-  char i;
-  
-
-  
-  for (i = 'a'; i <= 'z'; i++)
-    
-    putchar(i);
-  
-
-  
-  putchar('\n');
-  
-
-  
-  return (0);
-  
+char i;
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+return (0);
 }
