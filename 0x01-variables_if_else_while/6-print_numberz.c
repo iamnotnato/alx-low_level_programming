1d0c047d6017e585f3af897752dfdc19e10ef19c
Alan Nato <50413623+iamnotnato@users.noreply.github.com>
2022-12-10 07:24:12 -0600

    Betty Linter

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,11 +4,11 @@
  * Description : Prints all single digit numbers of base 10 starting from 0.
  * Return: Always 0.
  */
-int main(void)  
+int main(void)
 {
 int num;
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
 putchar('\n');
-return (0);  
+return (0);
 }
