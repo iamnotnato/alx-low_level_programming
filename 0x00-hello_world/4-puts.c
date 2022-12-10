9070523556617c5704f91c7fab1b82b0d4f66fd4
Alan Nato <50413623+iamnotnato@users.noreply.github.com>
2022-12-10 06:58:44 -0600

    Betty Linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * A C program that displays the words "Programming is like building a multilingual puzzle"
  */
 int main(void)
 {
