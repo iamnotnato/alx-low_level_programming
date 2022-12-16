f70bb4fd9dadabd1a5550475d2f417dc139dc536
Alan Nato <50413623+iamnotnato@users.noreply.github.com>
2022-12-16 04:09:14 +0300

    Betty

--- 1-alphabet.c
+++ 1-alphabet.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
