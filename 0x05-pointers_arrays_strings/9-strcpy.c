d7278fffcdd69377faf968884a0ee1d28a1109e0
Alan Nato <alanwnato@gmail.com>
2022-12-19 15:32:48 -0800

    Betty

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _strcpy - Copies a string pointed to by @src, including the
+* terminating null byte, to a buffer pointed to by @dest.
+* @dest: A buffer to copy the string to.
+* @src: The source string to copy.
+*
+* Return: A pointer to the destination string @dest.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+int l = 0;
+int x = 0;
+while (*(src + l) != '\0')
+{
+l++;
+}
+for ( ; x < l ; x++)
+{
+dest[x] = src[x];
+}
+dest[l] = '\0';
+return (dest);
+}
