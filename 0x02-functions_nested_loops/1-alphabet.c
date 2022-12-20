cb90501e1f4acf9f57e6d73384534b761555d435
Alan Nato <alanwnato@gmail.com>
2022-12-19 16:00:46 -0800

    Betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - check the code
- * Description : A function that prints the alphabet, in lowercase.
- * Return: Always 0.
- */
+* main - check the code
+* Description : A function that prints the alphabet, in lowercase.
+* Return: Always 0.
+*/
 void print_alphabet(void)
 {
 char letter;
