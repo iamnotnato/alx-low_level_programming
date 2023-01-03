c3da5a727c018cf20b5f0c24ddd14d55c7798576
Alan Nato <alanwnato@gmail.com>
2023-01-03 03:31:54 -0800

    101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,45 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
-* main - Generates random valid passwords for the
-* program 101-crackme.
-* Return: Always 0.
- */
+
 int main(void)
 {
-char password[84];
-int index = 0, sum = 0, diff_half1, diff_half2;
-srand(time(0));
-while (sum < 2772)
-{
-password[index] = 33 + rand() % 94;
-sum += password[index++];
-}
-password[index] = '\0';
-if (sum != 2772)
-{
-diff_half1 = (sum - 2772) / 2;
-diff_half2 = (sum - 2772) / 2;
-if ((sum - 2772) % 2 != 0)
-diff_half1++;
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half1))
-{
-password[index] -= diff_half1;
-break;
-}
-}
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half2))
-{
-password[index] -= diff_half2;
-break;
-}
-}
-}
-printf("%s", password);
-return (0);
+	int myrand;
+	int count;
+	int total;
+
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+
+	return (0);
 }
