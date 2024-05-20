132b6c1632c48e9ffad21bee5aef4e5b3034e8be
Alan Nato <50413623+iamnotnato@users.noreply.github.com>
2024-05-20 07:10:06 +0300

    fix : 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- * A program that displays text
+/*
+ * main - main function
+ * Return: 0
+ * A program that displays Programming is like building a multilingual puzzle
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
