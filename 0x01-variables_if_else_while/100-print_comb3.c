1b3711d96f534af698c26d8f995782d807f71666
Alan Nato <alanwnato@gmail.com>
2024-06-09 17:21:18 -0500

    fix : Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,15 +17,15 @@ while (i <= '9')
 	{
 		putchar(i);
 		putchar(j);
-	if (i != '8' || j != '9')
-        {
-		putchar(',');
-		putchar(' ');
-	}
+		if (i != '8' || j != '9')
+        	{
+			putchar(',');
+			putchar(' ');
+		}
 	j++;
 	}
 	i++;
-    }
+}
 putchar('\n');
 return (0);
 }
