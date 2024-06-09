31d81708e1c8b6231f6b1d83e3fa828ba7b6f546
Alan Nato <alanwnato@gmail.com>
2024-06-09 17:33:55 -0500

    fix : The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,27 +15,27 @@ int k;
 
 i = '0';
 while (i <= '7')
-  {
-    j = i + 1;
-    while (j <= '8')
-    {
-      k = j + 1;
-      while (k <= '9')
-      {
-        putchar(i);
-        putchar(j);
-        putchar(k);
-        if (i != '7' || j != '8' || k != '9')
-        {
-          putchar(',');
-          putchar(' ');
-        }
-        k++;
-      }
-      j++;
-    }
-    i++;
-  }
+{
+	j = i + 1;
+	while (j <= '8')
+	{
+	k = j + 1;
+	while (k <= '9')
+	{
+		putchar(i);
+		putchar(j);
+		putchar(k);
+		if (i != '7' || j != '8' || k != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		k++;
+	}
+	j++;
+	}
+	i++;
+}
 putchar('\n');
 return (0);
 }
