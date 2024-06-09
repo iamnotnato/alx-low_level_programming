4d6298594cebe04994594354b7ee7f04f7707216
Alan Nato <alanwnato@gmail.com>
2024-06-09 00:48:40 -0500

    fix : Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,8 @@
 
 /**
 * main - Entry point
-* Description : A program that assign a random number to the variable n each time it is executed
+* Description : A program that assign a random number
+* to the variable n each time it is executed
 * Return: Always 0 (Success)
 */
 
