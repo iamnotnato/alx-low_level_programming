801a34912dc604bc78d862ebf110ea985bcc1954
Alan Nato <alanwnato@gmail.com>
2024-06-09 17:31:04 -0500

    fix : The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
+
 /**
  * main - Entry Point
  * Description: Prints all single digit numbers of base 10 starting from 0.
  * Return: Always 0.
  */
+
 int main(void)
 {
+
 int i;
-for (i = '0'; i <= '9'; i++)
-{
-	putchar(i);
-}
+int j;
+int k;
+
+i = '0';
+while (i <= '7')
+  {
+    j = i + 1;
+    while (j <= '8')
+    {
+      k = j + 1;
+      while (k <= '9')
+      {
+        putchar(i);
+        putchar(j);
+        putchar(k);
+        if (i != '7' || j != '8' || k != '9')
+        {
+          putchar(',');
+          putchar(' ');
+        }
+        k++;
+      }
+      j++;
+    }
+    i++;
+  }
 putchar('\n');
 return (0);
 }
