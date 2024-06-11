15555ad11a40df40f329d9a8fd2952090ee7c2e8
Alan Nato <alanwnato@gmail.com>
2024-06-11 03:26:21 -0500

    fix :  I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- * main - Prints "_putchar" followed by a new line.
- *
+ * main - Entry Point
+ * Description: a program that prints _putchar.
  * Return: Always 0.
  */
-int main(void)
+
+int main()
 {
 _putchar('_');
 _putchar('p');
@@ -15,5 +17,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);
+return 0;
 }
