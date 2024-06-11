1dae832d18393e1c0d506d26175ddc1dc7a05279
Alan Nato <alanwnato@gmail.com>
2024-06-11 02:10:47 -0500

    fix : Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+
 /**
  * main - Entry Point
  * Description: Prints all single digit numbers of base 10 starting from 0.
  * Return: Always 0.
  */
+
 int main(void)
 {
 int i;
+
 for (i = '0'; i <= '9'; i++)
 {
-putchar(i);
-if (i != '9')
-{
+	putchar(i);
+	if (i != '9')
+	{
 	putchar(',');
 	putchar(' ');
-}
+	}
 }
 putchar('\n');
 return (0);
