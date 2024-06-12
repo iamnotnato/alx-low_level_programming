0995c0146eb5ddf0862809bedf4e195b9e135d81
Alan Nato <alanwnato@gmail.com>
2024-06-12 06:22:53 -0500

    fix :  Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+
 /**
-* main - main function
-* return 0
-* program prints with proper grammar, but the outcome is a piece of art
-* followed by a new line.
-*/
+ * main - Entry Point
+ * Description: a program that prints
+ * exactly with proper grammar,
+ * but the outcome is a piece of art,
+ * followed by a new line
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
