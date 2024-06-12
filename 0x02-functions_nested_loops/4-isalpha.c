4675bd05bcd0d997e7173c4a28ee296360bd5e9f
Alan Nato <alanwnato@gmail.com>
2024-06-12 04:32:45 -0500

    fix : isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
-* _isalpha - Checks if a character is alphabetic.
-* @c: The character to be checked.
-*
-* Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
-*/
+ * int _isalpha - Entry Point
+ * @c : parameter
+ * Description: a function that
+ * checks for lowercase character
+ * Return: Always 0.
+ */
+
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return (1);
-else
-return (0);
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+int _isalpha(int c);
 int _islower(int c);
 void print_alphabet_x10(void);
 void print_alphabet(void);
