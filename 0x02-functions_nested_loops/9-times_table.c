bdfa3077523700fc667a2fab94736c961f5a010d
Alan Nato <alanwnato@gmail.com>
2024-06-21 02:59:57 -0500

    fix : Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,24 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* times_table - Prints the 9 times table, starting with 0.
+* void times_table - prints the 9 times table,
+* starting with 0.
+* @void: The number in question.
+*
+* Return: void
 */
+
 void times_table(void)
 {
-int num, mult, prod;
-for (num = 0; num <= 9; num++)
-{
-_putchar('0');
-for (mult = 1; mult <= 9; mult++)
-{
-_putchar(',');
-_putchar(' ');
-prod = num * mult;
-if (prod <= 9)
-_putchar(' ');
-else
-_putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');
-}
-_putchar('\n');
-}
+int i;
+int j;
+
+for (i = 0; i <= 9; i++)
+	{
+	for (j = 0;j <= 9; j++)
+	{
+		if (j != 9)
+		{
+		printf("%d", i * j);
+		putchar(',');
+		putchar(' ');
+		}
+		else
+		{
+		printf("%d", i * j);
+		}
+	}
+
+	printf("\n");
+	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+void times_table(void);
 void jack_bauer(void);
 int print_last_digit(int);
 int _abs(int);
