aac074ab051b4effd94b90ac7ea61687f727642a
Alan Nato <alanwnato@gmail.com>
2024-06-21 03:24:28 -0500

    fix : Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,16 +16,16 @@ int j;
 for (i = 0; i <= 9; i++)
 	{
 	for (j = 0; j <= 9; j++)
-	{
-	if (j != 9)
-	{
-	printf("%2d, ", i * j);
-	}
-	else
-	{
-	printf("%2d", i * j);
-	}
-	}
+		{
+			if (j != 9)
+			{
+			printf("%2d, ", i * j);
+			}
+			else
+			{
+			printf("%2d", i * j);
+			}
+		}
 	printf("\n");
 	}
 }
