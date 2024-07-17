dd31d101542ce44970a130ec0cb18de10d2fce50
Alan Nato <alanwnato@gmail.com>
2024-07-17 09:44:04 -0500

    feat : Work in progress!

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,23 +1,24 @@
 #include "main.h"
 #include <stdio.h>
-#include <math.h>
 /**
-* main - main block
-* Description: Print the sum of all multiples of 3 or 5 below 1024.
-* Return: 0
+* print_natural_numbers - program that computes and prints
+* the sum of all the multiples of 3 or 5 below 1024
+* @void: The number in question.
+*
+* Return: void
 */
-int main(void)
+
+void print_natural_numbers(int n)
 {
-int i = 1;
-int total = 0;
-while (i < 1024)
+int k = 5;
+int m = 3;
+
+multiples = 10;
+for (i = 0; i < n; i++)
 {
-if (i % 3 == 0)
-total += i;
-else if (i % 5 == 0)
-total += i;
-i++;
+	if (multiples % n == 0)
+	{
+	putchar(i);
+	}
 }
-printf("%d\n", total);
-return (0);
 }
